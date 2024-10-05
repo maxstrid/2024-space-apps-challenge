8a5fb9b15f4d9292cae3ec8bbb8710838bb1cfe7
justinT21 <jjturcot@gmail.com>
2024-10-05 14:01:14 -0700

    Add initial latex paper
    Signed-off-by: justinT21 <jjturcot@gmail.com>

---
+++ Makefile
@@ -0,0 +1,5 @@
+all:
+	pdflatex -halt-on-error seismic_model.tex
+	bibtex seismic_model
+	pdflatex -halt-on-error seismic_model.tex
+	pdflatex -halt-on-error seismic_model.tex
--- mhe_example.py
+++ mhe_example.py
@@ -4,20 +4,20 @@ import matplotlib.pyplot as plt
 import time
 from casadi.tools import *
 from scipy import linalg
-plt.interactive(True)
+# plt.interactive(True)
 
 NP.random.seed(0)
 
 # Settings of the filter
-N = 10 # Horizon length
+N = 100000 # Horizon length
 dt = 0.05; # Time step
 
-sigma_p = 0.005 # Standard deviation of the position measurements
+sigma_p = 0.5 # Standard deviation of the position measurements
 sigma_w = 0.1 # Standard deviation for the process noise
 R = DM(1/sigma_p**2) # resulting weighting matrix for the position measurements
 Q = DM(1/sigma_w**2) # resulting weighting matrix for the process noise
 
-Nsimulation = 1000 # Lenght of the simulation
+Nsimulation = 100000 # Lenght of the simulation
 
 # Parameters of the system
 m = 1 # The weight of the mass
@@ -80,16 +80,16 @@ obj = 0
 obj += mtimes([(shooting["X",0]-parameters["x0"]).T,S,(shooting["X",0]-parameters["x0"])])
 #Next the cost for the measurement noise
 for i in range(N):
-  vm = h(shooting["X",i])-parameters["Y",i]
-  obj += mtimes([vm.T,R,vm])
+    vm = h(shooting["X",i])-parameters["Y",i]
+    obj += mtimes([vm.T,R,vm])
 #And also the cost for the process noise
 for i in range(N-1):
-  obj += mtimes([shooting["W",i].T,Q,shooting["W",i]])
+    obj += mtimes([shooting["W",i].T,Q,shooting["W",i]])
 
 # Build the multiple shooting constraints
 g = []
 for i in range(N-1):
-  g.append( shooting["X",i+1] - phi(shooting["X",i],parameters["U",i],shooting["W",i]) )
+    g.append( shooting["X",i+1] - phi(shooting["X",i],parameters["U",i],shooting["W",i]) )
 
 # Formulate the NLP
 nlp = {'x':shooting, 'p':parameters, 'f':obj, 'g':vertcat(*g)}
@@ -102,11 +102,11 @@ simulated_U = DM(cos(t[0:-1])).T # control input for the simulation
 simulated_U[:,int(Nsimulation/2):] = 0.0
 simulated_W = DM(sigma_w*NP.random.randn(Ndisturbances,Nsimulation-1)) # Process noise for the simulation
 for i in range(Nsimulation-1):
-  simulated_X[:,i+1] = phi(simulated_X[:,i], simulated_U[:,i], simulated_W[:,i])
+    simulated_X[:,i+1] = phi(simulated_X[:,i], simulated_U[:,i], simulated_W[:,i])
 #Create the measurements from these states
 simulated_Y = DM.zeros(Nmeas,Nsimulation) # Holder for the measurements
 for i in range(Nsimulation):
-  simulated_Y[:,i] = h(simulated_X[:,i])
+    simulated_Y[:,i] = h(simulated_X[:,i])
 # Add noise the the position measurements
 simulated_Y += sigma_p*NP.random.randn(simulated_Y.shape[0],simulated_Y.shape[1])
 
@@ -115,7 +115,8 @@ sigma_x0 = 0.01
 P = sigma_x0**2*DM.eye(Nstates)
 x0 = simulated_X[:,0] + sigma_x0*NP.random.randn(Nstates,1)
 # Create the solver
-opts = {"ipopt.print_level":0, "print_time": False, 'ipopt.max_iter':100}
+# opts = {"ipopt.print_level":6, "print_time": True, 'ipopt.max_iter':100}
+opts = {}
 nlpsol = nlpsol("nlpsol", "ipopt", nlp, opts)
 
 # Create a holder for the estimated states and disturbances
@@ -139,43 +140,44 @@ estimated_W[:,0:N-1] = solution["W",lambda x: horzcat(*x)]
 # Now make a loop for the rest of the simulation
 for i in range(1,Nsimulation-N+1):
 
-  # Update the arrival cost, using linearisations around the estimate of MHE at the beginning of the horizon (according to the 'Smoothed EKF Update'): first update the state and covariance with the measurement that will be deleted, and next propagate the state and covariance because of the shifting of the horizon
-  print("step %d/%d (%s)" % (i, Nsimulation-N , nlpsol.stats()["return_status"]))
-  H0 = H(solution["X",0])
-  K = mtimes([P,H0.T,linalg.inv(mtimes([H0,P,H0.T])+R)])
-  P = mtimes((DM.eye(Nstates)-mtimes(K,H0)),P)
-  h0 = h(solution["X",0])
-  x0 = x0 + mtimes(K, current_parameters["Y",0]-h0-mtimes(H0,x0-solution["X",0]))
-  x0 = phi(x0, current_parameters["U",0], solution["W",0])
-  F = PHI(solution["X",0], current_parameters["U",0], solution["W",0])
-  P = mtimes([F,P,F.T]) + linalg.inv(Q)
-  # Get the measurements and control inputs
-  current_parameters["U",lambda x: horzcat(*x)] = simulated_U[:,i:i+N-1]
-  current_parameters["Y",lambda x: horzcat(*x)] = simulated_Y[:,i:i+N]
-  current_parameters["S"] = linalg.inv(P)
-  current_parameters["x0"] = x0
-  # Initialize the system with the shifted solution
-  initialisation_state["W",lambda x: horzcat(*x),0:N-2] = estimated_W[:,i:i+N-2] # The shifted solution for the disturbances
-  initialisation_state["W",N-2] = DM.zeros(Ndisturbances,1) # The last node for the disturbances is initialized with zeros
-  initialisation_state["X",lambda x: horzcat(*x),0:N-1] = estimated_X[:,i:i+N-1] # The shifted solution for the state estimates
-  # The last node for the state is initialized with a forward simulation
-  phi0 = phi(initialisation_state["X",N-1], current_parameters["U",-1], initialisation_state["W",-1])
-  initialisation_state["X",N-1] = phi0
-  # And now initialize the solver and solve the problem
-  res = nlpsol(p=current_parameters, x0=initialisation_state, lbg=0, ubg=0)
-  solution = shooting(res["x"])
+    # Update the arrival cost, using linearisations around the estimate of MHE at the beginning of the horizon (according to the 'Smoothed EKF Update'): first update the state and covariance with the measurement that will be deleted, and next propagate the state and covariance because of the shifting of the horizon
+    print("step %d/%d (%s)" % (i, Nsimulation-N , nlpsol.stats()["return_status"]))
+    H0 = H(solution["X",0])
+    K = mtimes([P,H0.T,linalg.inv(mtimes([H0,P,H0.T])+R)])
+    P = mtimes((DM.eye(Nstates)-mtimes(K,H0)),P)
+    h0 = h(solution["X",0])
+    x0 = x0 + mtimes(K, current_parameters["Y",0]-h0-mtimes(H0,x0-solution["X",0]))
+    x0 = phi(x0, current_parameters["U",0], solution["W",0])
+    F = PHI(solution["X",0], current_parameters["U",0], solution["W",0])
+    P = mtimes([F,P,F.T]) + linalg.inv(Q)
+    # Get the measurements and control inputs
+    current_parameters["U",lambda x: horzcat(*x)] = simulated_U[:,i:i+N-1]
+    current_parameters["Y",lambda x: horzcat(*x)] = simulated_Y[:,i:i+N]
+    current_parameters["S"] = linalg.inv(P)
+    current_parameters["x0"] = x0
+    # Initialize the system with the shifted solution
+    initialisation_state["W",lambda x: horzcat(*x),0:N-2] = estimated_W[:,i:i+N-2] # The shifted solution for the disturbances
+    initialisation_state["W",N-2] = DM.zeros(Ndisturbances,1) # The last node for the disturbances is initialized with zeros
+    initialisation_state["X",lambda x: horzcat(*x),0:N-1] = estimated_X[:,i:i+N-1] # The shifted solution for the state estimates
+    # The last node for the state is initialized with a forward simulation
+    phi0 = phi(initialisation_state["X",N-1], current_parameters["U",-1], initialisation_state["W",-1])
+    initialisation_state["X",N-1] = phi0
+    # And now initialize the solver and solve the problem
+    res = nlpsol(p=current_parameters, x0=initialisation_state, lbg=0, ubg=0)
+    solution = shooting(res["x"])
 
-  # Now get the state estimate. Note that we are only interested in the last node of the horizon
-  estimated_X[:,N-1+i] = solution["X",N-1]
-  estimated_W[:,N-2+i] = solution["W",N-2]
+    # Now get the state estimate. Note that we are only interested in the last node of the horizon
+    estimated_X[:,N-1+i] = solution["X",N-1]
+    estimated_W[:,N-2+i] = solution["W",N-2]
 # Plot the results
 plt.figure(1)
 plt.clf()
 plt.plot(t,vec(estimated_X[0,:]),'b--')
-plt.plot(t,vec(simulated_X[0,:]),'r--')
+plt.plot(t, vec(simulated_X[0, :]), 'r--')
+plt.plot(t, vec(simulated_Y[0, :]), 'y--')
 plt.title("Position")
 plt.xlabel('Time')
-plt.legend(['Estimated position','Real position'])
+plt.legend(['Estimated position','Real position', 'Measured Position'])
 plt.grid()
 
 plt.figure(2)
@@ -190,4 +192,4 @@ plt.show()
 
 error = estimated_X[0,:]-simulated_X[0,:]
 print(mtimes(error,error.T))
-assert(mtimes(error,error.T)<0.01)
+assert (mtimes(error, error.T) < 0.01)
