08bf8855c83be24990379cc28a1410ff7197fd57
ThePersonOfStuff <maxf6.283@gmail.com>
2024-10-06 14:03:47 -0700

    ASDASDASDASD

--- src/mhe_test_data.py
+++ src/mhe_test_data.py
@@ -5,15 +5,15 @@ import mhe
 import data_parsing
 
 data_reader = data_parsing.DataReader()
-data = data_reader.read(10, n_max_subsections=50, n_max_sections=1)
+data = data_reader.read(0, n_max_subsections=50, n_max_sections=1)
 
 mhe = mhe.MHE(30, dt=data.delta,
               ground_spring_constant=30.93226074688124,
-              ground_damping_constant=0.08,
+              ground_damping_constant=0.05,
               force_cost=0.000001,
               rk_error_cost=100000000,
               measured_error_cost=1000,
-              force_variance_cost=0.00008,
+              force_variance_cost=0.0001,
               force_variance_variance_cost=0)
 
 min_v, max_v = data.max_ranges[0]
