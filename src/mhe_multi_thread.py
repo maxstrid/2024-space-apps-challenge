f1e3c266275d4579247c17e6970825f5c2d98996
justinT21 <jjturcot@gmail.com>
2024-10-06 15:15:47 -0700

    Fix plotting in multithreading

--- src/mhe_multi_thread.py
+++ src/mhe_multi_thread.py
@@ -10,10 +10,6 @@ import time
 cpu_count = os.cpu_count() - 10
 print(cpu_count)
 
-fig = [None] * cpu_count
-ax = [None] * cpu_count
-
-
 def run(i):
     try:
         data_reader = data_parsing.DataReader()
@@ -27,21 +23,18 @@ def run(i):
                       force_variance_cost=10000,
                       force_variance_variance_cost=100000)
         min_v, max_v = data.max_ranges[0]
-        mhe.read(data.velocity[0:100])
+        mhe.read(data.velocity[min_v:max_v])
         mhe.solve_all(i)
-        fig[i], ax[i] = mhe.plot(plot=False)
-        ax[i].plot(np.linspace(0, -data.time[0] + data.time[100], mhe.n),
-                   data.velocity[0:100],
+        fig, ax = mhe.plot(plot=False)
+        ax.plot(np.linspace(0, -data.time[min_v] + data.time[max_v], mhe.n),
+                   data.velocity[min_v:max_v],
                    label="x")
-        ax[i].legend()
+        ax.legend()
+        fig.savefig(f"temp{i}.png")
     except Exception as e:
         print(e)
         print(tb=traceback.format_exc())
 
 
-result = []
 with ProcessPoolExecutor(max_workers=cpu_count) as exe:
-    result = exe.map(run, [i for i in range(cpu_count)])
-
-print(result)
-plt.show()
+    exe.map(run, [i for i in range(cpu_count)])
