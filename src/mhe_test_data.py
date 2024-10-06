ab1efb8fe38d87ac6cf7961a284e737c14ce51d4
ThePersonOfStuff <maxf6.283@gmail.com>
2024-10-06 10:34:41 -0700

    asd

--- src/mhe_test_data.py
+++ src/mhe_test_data.py
@@ -5,13 +5,14 @@ import mhe
 import data_parsing
 
 data_reader = data_parsing.DataReader()
-data = data_reader.read(0, n_max_subsections=100, n_max_sections=1)
+data = data_reader.read(0, n_max_subsections=50, n_max_sections=1)
 
 mhe = mhe.MHE(50, dt=data.delta,
               ground_spring_constant=30.93,
+              ground_damping_constant=0.01,
               force_cost=1,
               rk_error_cost=10000000000,
-              measured_error_cost=10000,
+              measured_error_cost=100000,
               force_variance_cost=10000,
               force_variance_variance_cost=100000)
 
@@ -20,7 +21,8 @@ min_v, max_v = data.max_ranges[0]
 mhe.read(data.velocity[min_v:max_v])
 mhe.solve_all()
 fig, ax = mhe.plot(plot=False)
-ax.plot(np.linspace(0, -data.time[min_v]+data.time[max_v], mhe.n), data.velocity[min_v:max_v], label="x")
-ax.legend()
+ax[0].plot(np.linspace(0, -data.time[min_v]+data.time[max_v], mhe.n), data.velocity[min_v:max_v], label="x")
+ax[0].legend()
+ax[1].legend()
 
 plt.show()
