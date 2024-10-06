42b6a4615e5b789e5566f116adc1c31e3af687ea
ThePersonOfStuff <maxf6.283@gmail.com>
2024-10-06 16:12:35 -0700

    ASDASDASD

--- src/mhe_multi_thread.py
+++ src/mhe_multi_thread.py
@@ -6,6 +6,7 @@ import data_parsing
 import os
 from concurrent.futures import *
 import time
+import pandas as pd
 
 cpu_count = os.cpu_count() - 10
 print(cpu_count)
@@ -14,14 +15,14 @@ def run(i):
     try:
         data_reader = data_parsing.DataReader()
         data = data_reader.read(i, n_max_subsections=10, n_max_sections=1)
-        mhe = MHE.MHE(20,
-                      dt=data.delta,
-                      ground_spring_constant=30.93,
-                      force_cost=1,
-                      rk_error_cost=10000000000,
-                      measured_error_cost=10000,
-                      force_variance_cost=10000,
-                      force_variance_variance_cost=100000)
+        mhe = MHE.MHE(30, dt=data.delta,
+                        ground_spring_constant=30.93226074688124,
+                        ground_damping_constant=0.05,
+                        force_cost=0.000001,
+                        rk_error_cost=100000000,
+                        measured_error_cost=1000,
+                        force_variance_cost=0.0001,
+                        force_variance_variance_cost=0)
         min_v, max_v = data.max_ranges[0]
         mhe.read(data.velocity[min_v:max_v])
         mhe.solve_all(i)
@@ -31,6 +32,13 @@ def run(i):
                    label="x")
         ax.legend()
         fig.savefig(f"temp{i}.png")
+        df = pd.DataFrame()
+        df['x-est'] = pd.Series(mhe.x_est[:,0])
+        df['y-est'] = pd.Series(mhe.x_est[:,1])
+        df['f-est'] = pd.Series(mhe.f_est)
+        df.to_csv('data_' + str(i) + '.csv')
+
+
     except Exception as e:
         print(e)
         print(tb=traceback.format_exc())
--- src/mhe_test_data.py
+++ src/mhe_test_data.py
@@ -3,9 +3,11 @@ import numpy as np
 import matplotlib.pylab as plt
 import mhe
 import data_parsing
+import pandas as pd
 
 data_reader = data_parsing.DataReader()
-data = data_reader.read(0, n_max_subsections=50, n_max_sections=1)
+n_data = 0
+data = data_reader.read(n_data, n_max_subsections=10, n_max_sections=1)
 
 mhe = mhe.MHE(30, dt=data.delta,
               ground_spring_constant=30.93226074688124,
@@ -20,6 +22,14 @@ min_v, max_v = data.max_ranges[0]
 
 mhe.read(data.velocity[min_v:max_v])
 mhe.solve_all()
+
+df = pd.DataFrame()
+df['x-est'] = pd.Series(mhe.x_est[:,0])
+df['y-est'] = pd.Series(mhe.x_est[:,1])
+df['f-est'] = pd.Series(mhe.f_est)
+df.to_csv('data_' + str(n_data) + '.csv')
+
+
 fig, ax = mhe.plot(plot=False)
 ax[0].plot(np.linspace(0, -data.time[min_v]+data.time[max_v], mhe.n), data.velocity[min_v:max_v], label="x")
 ax[0].legend()
