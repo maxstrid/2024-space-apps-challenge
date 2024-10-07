41efe00a20457258fe583fb880add04a9b2be66e
ThePersonOfStuff <maxf6.283@gmail.com>
2024-10-06 19:04:29 -0700

    analysis

--- src/examine_data.py
+++ src/examine_data.py
@@ -10,6 +10,7 @@ data = pd.read_csv("data_0.csv")
 x = np.linspace(0, 0.15 * len(data['f-est']), len(data['f-est']))
 
 #take progressive rolling averages
+axes: list[plt.Axes]
 fig, axes = plt.subplots(4)
 axes[0].plot(data['f-est'], label="raw")
 
@@ -33,14 +34,15 @@ def roll_avg(data: np.ndarray, rnge: int):
 # axes[2].plot(x, roll_avg(np.sqrt(data['f-est']), 10), label="rt10")
 # axes[3].plot(x, roll_avg(data['f-est'], 100), label="100")
 d = data['f-est'][:-1]
-for i in range(20):
-    d = roll_avg(d, 100)
+for i in range(10):
+    d = roll_avg(d, 1000)
 
-axes[1].plot(d, label="short-term")
+axes[1].plot(d, label="short-term", linewidth=5.0)
+
 
 d2 = d.copy()
-for i in range(20):
-    d2 = roll_avg(d, 1000)
+for i in range(10):
+    d2 = roll_avg(d, 10000)
 
 axes[2].plot(d2, label="long-term")
 
